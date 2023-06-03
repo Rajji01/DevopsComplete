52ed2fd9f2fd52b3fcfbdad4451d2f1ebace4857
Rajji01 <=rajatagrawal2702@gmail.com>
2023-06-03 11:21:02 -0700

    second commit

---
+++ Devops/src/main/java/com/backend/Devops/DevopsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApplication.class, args);
+	}
+
+}
